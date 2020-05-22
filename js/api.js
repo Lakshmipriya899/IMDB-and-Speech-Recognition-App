d8a164553e1c6b87c9b0e77c6030f5a298be7552
Lakshmipriya <samantaraylakshmipriya.97@gmail.com>
2020-05-22 21:34:54 +0530

    we are learning git and github

---
+++ js/api.js
@@ -0,0 +1,28 @@
+let githubServer = "https://api.github.com/users";
+
+// window
+// .fetch(githubServer)
+// .then((data) => console.log(data))
+// .catch((err) => console.log(err));
+
+//-----------------------------------------
+
+window.fetch(githubServer)
+.then((data) => {
+    data.json().then(gitUsers => {
+       let output = [];
+       for(let user of gitUsers) {
+           output +=`
+           <div>
+               <img src ="${user.avatar_url}" />
+               <h1>user : ${user.login}</h1>
+               <a href = "${user.html_url}" target ="_blank">details</a>
+           </div>
+           `;
+       }
+       document.getElementById("template").innerHTML = output;
+    
+    })
+    .catch(err => console.log(err));
+})
+.catch((err) => console.log(err));
